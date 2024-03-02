eaffbb1e49e76004d042715ea38be55f992b310b
flyandancexo <66555404+flyandancexo@users.noreply.github.com>
2024-03-02 06:39:46 -0800

    Create Main.c

---
+++ Firmware_V1.62/Main.c
@@ -0,0 +1,19 @@
+#define F_CPU 8000000UL  // 8 MHz
+#include <util/delay.h>
+
+#include <inttypes.h>
+#include <avr/io.h>
+
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+
+#include "xFun.h"
+ 
+int main(void){
+
+  while(1){
+
+  }//--while
+
+}//--main 
