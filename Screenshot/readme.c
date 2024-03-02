177faa41247071aee74b106f3f5150abac89c036
flyandancexo <66555404+flyandancexo@users.noreply.github.com>
2024-03-02 06:37:59 -0800

    Update and rename readme.md to readme.c

---
+++ Screenshot/readme.c
@@ -0,0 +1,15 @@
+## FDxboot V1.62 Speed Test Results - Loosely based on AVR109
+| Baud Rate | MCU | Page Size | File Size | Write Time | Upload Speed | Read Time | Download Speed |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| **2Mbps** | Atmega128 | 256 | 129,998 | 2.83s | **45,935 B/s** | 2.54s | **51,180 B/s** | 
+| 1Mbps | Atmega128 | 256 | 129,998 | 3.36s | 38,689 B/s | 3.05s | 42,622 B/s | 
+| 500kbps | Atmega128 | 256 | 129,998 | 4.57s | 28,445 B/s | 4.07s | 31,940 B/s | 
+| **2Mbps** | Atmega328p | 128 | 31,736 | 1.20s | **26,446 B/s** | 0.99s | **32,056 B/s** | 
+| 1Mbps | Atmega328p | 128 | 31,736 | 1.37s | 23,164 B/s | 0.99s | 32,056 B/s | 
+| 500kbps | Atmega328p | 128 | 31,736 | 1.63s | 19,469 B/s | 1.24s | 25,593 B/s | 
+| **2Mbps** | Atmega16 | 128 | 15,352 | 0.57s | **26,933 B/s** | 0.48s | **31,983 B/s** | 
+| 1Mbps | Atmega16 | 128 | 15,352 | 0.65s | 23,618 B/s | 0.48s | 31,983 B/s | 
+| 500kbps | Atmega16 | 128 | 15,352 | 0.77s | 19,937 B/s | 0.6s | 25,586 B/s | 
+| **2Mbps** | Atmega88 | 64 | 6,102 | 0.43s | **14,190 B/s** | 0.29s | **21,041 B/s** | 
+| 1Mbps | Atmega88 | 64 | 6,102 | 0.43s | 14,190 B/s | 0.29s | 21,041 B/s | 
+| 500kbps | Atmega88 | 64 | 6,102 | 0.48s | 12,712 B/s | 0.39s | 15,646 B/s | 
